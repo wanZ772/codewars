72b190350409bb904372a42456c019b1b61ccf11
Muhammad Najwan <najwanhassan03@gmail.com>
2024-04-06 14:28:32 +0800

    array_diff

---
+++ python/find_next_perfect_square.py
@@ -0,0 +1,11 @@
+from math import sqrt
+
+def find_next_square(sq):
+    if sq > 0 and sq % sqrt(sq) == 0:
+        sq += 1
+        while sq % sqrt(sq) != 0:
+            sq += 1
+        return sq
+    return -1
+
+print(find_next_square(0))
